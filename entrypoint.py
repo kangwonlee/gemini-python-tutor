53026118d140a3494e358298982594f47fa5fb29
KangWon LEE <kangwon@gmail.com>
2024-10-01 18:51:38 +0900

    call main()

--- entrypoint.py
+++ entrypoint.py
@@ -42,18 +42,4 @@ def get_path_tuple(report_files_str:str) -> Tuple[pathlib.Path]:
 
 
 if __name__ == "__main__" :
-    # Rename these variables to something meaningful
-    input1 = sys.argv[1]
-    input2 = sys.argv[2]
-
-
-    # Fake example outputs
-    output1 = "Hello"
-    output2 = "World"
-
-    # This is how you produce workflow outputs.
-    # Make sure corresponds to output variable names in action.yml
-    if "GITHUB_OUTPUT" in os.environ :
-        with open(os.environ["GITHUB_OUTPUT"], "a") as f :
-            print("{0}={1}".format("output-one", output1), file=f)
-            print("{0}={1}".format("output-two", output2), file=f)
+    main()
