01ecd98e67381ce8d524be9b6a4e675d6cadb657
KangWon LEE <kangwon@gmail.com>
2024-10-01 18:56:09 +0900

    remove unused functions

--- ai_tutor.py
+++ ai_tutor.py
@@ -23,55 +23,7 @@ RESOURCE_EXHAUSTED = 429
 
 
 def test_API_key():
-    if not GOOGLE_API_KEY:
-        pytest.fail('API KEY NOT Available')
-
-
-def tutor_folder() -> pathlib.Path:
-    p = pathlib.Path(__file__).parent.resolve()
-    assert p.exists()
-    assert p.is_dir()
-    return p
-
-
-def proj_folder(tutor_folder_path:pathlib.Path=tutor_folder()) -> pathlib.Path:
-    p = tutor_folder_path.parent.resolve()
-    assert p.exists()
-    assert p.is_dir()
-    return p
-
-
-def script_path(proj_folder:pathlib.Path=proj_folder()) -> pathlib.Path:
-    '''
-    Automatically discover ex??.py file
-    Force only one ex??.py file in the project folder at the moment
-    '''
-    exercise_files = tuple(proj_folder.glob('ex*.py'))
-
-    result = None
-    if len(exercise_files) == 0:
-        raise FileNotFoundError("No Python file starting with 'ex' found in the project folder.")
-    elif len(exercise_files) > 1:
-        raise ValueError("Multiple Python files starting with 'ex' found in the project folder. Please ensure there is only one.")
-    else:
-        result = exercise_files[0]
-
-    return result
-
-
-@pytest.fixture
-def report_paths() -> Tuple[pathlib.Path]:
-    result = []
-    for e_key in os.environ:
-        if e_key.endswith('_REPORT'):
-            report_path = pathlib.Path(os.environ[e_key])
-            assert report_path.exists(), f"does not exist : {report_path}"
-            assert report_path.is_file(), f"not a file : {report_path}"
-            result.append(report_path)
-
-    assert result, "Could not find report filenames"
-
-    return tuple(result)
+    assert GOOGLE_API_KEY, 'API KEY NOT Available'
 
 
 @functools.lru_cache
