a4e2f11f7916ebc072578a5fa86d09ead80fe4ae
KangWon LEE <kangwon@gmail.com>
2024-10-02 10:39:44 +0900

    read from GITHUB_OUTPUT
    set-output depricated

--- tests/integration.py
+++ tests/integration.py
@@ -8,12 +8,34 @@
 # and then after that, run these integration
 # tests.
 
+import logging
 import os
 
 
 import pytest
 
 
+logging.basicConfig(level=logging.INFO)
+
+
+def get_github_output(output_name:str) -> str:
+    '''
+    Reads the GITHUB_OUTPUT environment file and returns the value of the specified output.
+    '''
+    result = None
+
+    with open(os.environ['GITHUB_OUTPUT'], 'r') as f:
+        for line in f:
+            key, value = line.strip().split('=')
+            if key == output_name:
+                result = value
+
+    if result is None:
+        logging.warning(f'Output {output_name} not found in GITHUB_OUTPUT file')
+
+    return result  # Or raise an exception if the output is not found
+
+
 def test_feedback() -> None:
-    feedback_str = os.getenv('FEEDBACK')
+    feedback_str = get_github_output('feedback')
     assert feedback_str, 'No feedback found'
